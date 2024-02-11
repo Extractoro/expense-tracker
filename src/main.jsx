94342e20d337b8d443736a50964a4a87afcffb5f
Vadym <67538058+Extractoro@users.noreply.github.com>
2024-02-11 12:15:56 +0200

    fix: base url

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/expanse-tracker-extractoro.netlify.app/">
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <App />
